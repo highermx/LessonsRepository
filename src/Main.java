2ece12ed0a59ab8cc1dc75253545d78008e555d0
Maxim Dyachenko <onedayexperience808@gmail.com>
2025-06-29 12:42:27 +0700

    Homework lesson-7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        Employee employee1 = new Employee("John", "Java developer", "johnjava@gmail.com", 795195475, 31);
+        System.out.println(employee1);
+
+        Car car = new Car();
+        car.start();
 
     }
 }
