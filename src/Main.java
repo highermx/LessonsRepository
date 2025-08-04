bfd42cb01eb2ea152ab9ecc2456ef199ece9f916
Maxim Dyachenko <onedayexperience808@gmail.com>
2025-08-04 21:47:30 +0700

    Homework lesson-15

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import iprody.coffeeshot.CoffeeOrderBoard;
+
 public class Main {
     public static void main(String[] args) {
+        CoffeeOrderBoard board = new CoffeeOrderBoard();
+
+        board.add("Alina");
+        board.add("Boris");
+        board.add("Anna");
+
+        System.out.println("Текущая очередь:");
+        board.draw();
+
+        System.out.println("\nВыдан заказ: " + board.deliver());
+        System.out.println("\nОчередь после выдачи:");
+        board.draw();
+
+        System.out.println("\nВыдан заказ №3: " + board.deliver(3));
+        System.out.println("\nФинальная очередь:");
+        board.draw();
 
     }
 }
