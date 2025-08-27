af3cbabc202caedce7c031bb1caabeccf3da92a3
Maxim Dyachenko <onedayexperience808@gmail.com>
2025-08-27 17:07:42 +0700

    Homework lesson-19

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        FileLoggerConfiguration config = new FileLoggerConfiguration(
+                "C:\\Users\\Максим\\Desktop",
+                "Test.txt",
+                LoggingLevel.DEBUG,
+                1024,
+                "[%s][%s] %s");
+
+        Logger logger = new FileLogger(config);
+
+        logger.debug("Запуск приложения");
+        logger.info("Пользователь вошёл в систему");
+        logger.debug("Отправка запроса к БД");
 
     }
 }
