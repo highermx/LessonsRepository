a7c2175e8a7f2055bce56b89c73b0ea7dec39b81
Maxim Dyachenko <onedayexperience808@gmail.com>
2025-08-30 11:33:05 +0700

    Homework lesson-20

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        FileStorage storage = new FileStorage(Paths.get("/data/storage"));
+        storage.put("documents", "report.pdf", Paths.get("local_report.pdf"));
+
+        FileStorageReader reader = new FileStorageReader(storage);
+        byte[] fullContent = reader.read("documents", "report.pdf");
+        List<byte[]> chunks = reader.read("documents", "report.pdf", 4096);
 
     }
 }
